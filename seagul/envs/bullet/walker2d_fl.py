ac2a6ff860ff2c4d0e3c885967917a6fba8c4d44
sgillen <seangillen1887@gmail.com>
2019-08-21 17:30:52 -0700

    created env for bullet walker, initialized it with some drake trajectories
    Former-commit-id: 9b78a2a9f31d27ff2e65feffb058796e04345a01
    Former-commit-id: a48edaea3b2704d2318fcfe5a65b56aeef63d1e2 [formerly 76a65de41c5a173dfe8f66a0c36f0b79df75f9f8] [formerly 7101fbd4b2bf9b642f9d6044935c00047121e368 [formerly d3c142c9e8111e769041ac143e5dabbaaae59ba9]]
    Former-commit-id: 5518861b443524dde36402cea0a6979d19bb262b [formerly 74df71965b04cc3e86a167a413a3793bab4cc32b]
    Former-commit-id: 5bc5c707ad947cccc8a66205a4b8cd9844d7e6d1

---
+++ seagul/envs/bullet/walker2d_fl.py
@@ -0,0 +1,42 @@
+from pybullet_envs.robot_bases import XmlBasedRobot, MJCFBasedRobot, URDFBasedRobot
+from pybullet_envs.robot_locomotors import WalkerBase
+from pybullet_envs.gym_locomotion_envs import WalkerBaseBulletEnv
+from seagul.resources import getResourcePath
+from numpy import pi
+
+class Walker2DFiveLinkBase(WalkerBase):
+    foot_list = ["leg", "leg_left"]
+
+    def __init__(self):
+        WalkerBase.__init__(self, getResourcePath() + "/Walker2d_five.xml", "torso", action_dim=4, obs_dim=18, power=.40)
+        self.init_pos = [-0.86647779, -5.57969548,  4.56618282, -0.86647779]
+        self.init_vel = [-0.08985754,  2.59193943, -0.48066481,  1.88797459]
+
+    def alive_bonus(self, z, pitch):
+        return +1 if z > 0.8 and abs(pitch) < 1.0 else -1
+
+    def robot_specific_reset(self, bullet_client):
+        WalkerBase.robot_specific_reset(self, bullet_client)
+
+
+
+        self.jdict['leg_joint'].set_position(self.init_pos[0]*180/pi)
+        self.jdict['thigh_joint'].set_position(self.init_pos[2]*180/pi)
+        self.jdict['leg_left_joint'].set_position(-self.init_pos[1]*180/pi)
+        self.jdict['thigh_left_joint'].set_position(self.init_pos[3]*180/pi)
+
+        # self.jdict['leg_joint'].set_position(-50)
+        # self.jdict['leg_left_joint'].set_position(50)
+        #self.jdict['thigh_joint'].set_position(50)
+        #self.jdict['thigh_left_joint'].set_position(-50)
+
+
+        #for n in ["foot_joint", "foot_left_joint"]:
+        #    self.jdict[n].power_coef = 30.0
+
+class Walker2DFiveLink(WalkerBaseBulletEnv):
+    def __init__(self, render=False):
+        self.robot = Walker2DFiveLinkBase()
+        WalkerBaseBulletEnv.__init__(self, self.robot, render)
+
+
