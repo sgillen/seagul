746458c6d658aeb78a3ae50cdc650688fc7bf80e
sgillen <seangillen1887@gmail.com>
2019-08-22 14:10:30 -0700

    broke out some rl functions into a common file
    Former-commit-id: 3f420b536548cc4bbb24d5fc78021fe8cd0dfa10
    Former-commit-id: 0767bd0fe44bbedf4515a1d6b65383eef2edc707 [formerly cdfd810c9defdec0e0ebc6141ee61d3529fb08d9] [formerly 8d5c93e8d7b445f851a15f91e0790142bd98ab99 [formerly 429ad322e31ee8f16a3f065828fe6dd7db4fa42d]]
    Former-commit-id: bb98cef3012d96fa9118d528b9049c73a2748394 [formerly 5b2336b007b8981dc9d6c3305a0b1a2d4278a4c8]
    Former-commit-id: cd9cb27ea53bc45ea2589680da865fc173c49740

--- seagul/rl/ppo_switching.py
+++ seagul/rl/ppo_switching.py
@@ -4,13 +4,15 @@ import gym
 import seagul.envs
 import pybullet_envs
 
+from seagul.rl.common import select_cont_action, select_discrete_action, get_discrete_logp, get_cont_logp, discount_cumsum
+
 import numpy as np
 from tqdm import trange
 import copy
 
+
 import torch
 from torch.utils import data
-from torch.distributions import Normal, Categorical
 
 from numpy import pi, sin, cos
 
@@ -350,54 +352,6 @@ def ppo_switch(
 
     return (policy, value_fn, gate_fn, avg_reward_hist, locals())
 
-
-# helper functions
-# ============================================================================================
-
-# takes a policy and the states and sample an action from it... (can we make this faster?)
-def select_cont_action(policy, state, variance):
-    means = policy(torch.as_tensor(state)).squeeze()
-    m = Normal(loc=means, scale=torch.ones_like(means) * variance)
-    action = m.sample()
-    logprob = m.log_prob(action)
-    return action.detach(), logprob
-
-
-# given a policy plus a state/action pair, what is the log liklihood of having taken that action?
-def get_cont_logp(policy, states, actions, variance):
-    means = policy(torch.as_tensor(states)).squeeze()
-    m = Normal(loc=means, scale=torch.ones_like(means) * variance)
-    logprob = m.log_prob(actions)
-    return logprob
-
-
-# takes a policy and the states and sample an action from it... (can we make this faster?)
-def select_discrete_action(policy, state):
-    probs = torch.tensor([policy(torch.as_tensor(state)), 1 - policy(torch.as_tensor(state))])
-    m = Categorical(probs)
-    action = m.sample()
-    logprob = m.log_prob(action)
-    return action.detach(), logprob
-
-
-# given a policy plus a state/action pair, what is the log liklihood of having taken that action?
-def get_discrete_logp(policy, state, action):
-    probs = torch.cat((policy(torch.as_tensor(state)), 1 - policy(torch.as_tensor(state))), dim=1)
-    m = Categorical(probs)
-    if any(torch.isnan(state).flatten()):
-        print("whoops")
-    logprob = m.log_prob(action)
-    return logprob
-
-
-# can make this faster I think?
-def discount_cumsum(rewards, discount):
-    future_cumulative_reward = 0
-    cumulative_rewards = torch.empty_like(torch.as_tensor(rewards))
-    for i in range(len(rewards) - 1, -1, -1):
-        cumulative_rewards[i] = rewards[i] + discount * future_cumulative_reward
-        future_cumulative_reward = cumulative_rewards[i]
-    return cumulative_rewards
 
 
 def control(env, q):
