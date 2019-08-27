bugfix for normalization
Former-commit-id: 30aba8cdecd28066ae88c22f6e82dc5b572ef861
Former-commit-id: 34c6bd734e159c5891a95f2a029704c8bfd3e826 [formerly 1bf570ccdc21cf812fbb55263e0d051c0838ae04] [formerly e95bd2e55df420fc0516b2b45180f02c9553b846 [formerly 8cd2addda459886fe0cc0a2016faa9aa7583cb45]]
Former-commit-id: 2ad23ebb78850dae0ecdf7243d050002ceccf380 [formerly 0a86ad71f94593af5f79f05a5162501ac79f39fc]
Former-commit-id: 77351f3f081f354cc9d899f3729d01936a87cc5f

seagul/rl/ppo.py
@@ -176,8 +176,8 @@ def ppo(
             if batch_steps > epoch_batch_size:
 
 
-                state_mean = torch.mean(state_tensor, 0)*state_tensor.shape[0] + state_mean*num_states
-                state_var = torch.var(state_tensor, 0)*state_tensor.shape[0] + state_var*num_states
+                state_mean = (torch.mean(state_tensor, 0)*state_tensor.shape[0] + state_mean*num_states)/(state_tensor.shape[0] + num_states)
+                state_var = torch.var(state_tensor, 0)*state_tensor.shape[0] + state_var*num_states/(state_tensor.shape[0] + num_states)
 
                 policy.state_means = state_mean
                 policy.state_var = state_var
@@ -284,4 +284,4 @@ if __name__ == "__main__":
     eps = 0.2
 
     # env2, t_policy, t_val, rewards = ppo('InvertedPendulum-v2', 100, policy, value_fn)
-    env2, t_policy, t_val, rewards = ppo("su_cartpole-v0", 5, policy, value_fn)
+    env2, t_policy, t_val, rewards = ppo("su_cartpole-v0", 100, policy, value_fn)