a93a809bb3953c3f442c5e35a19a18e46c33fc55
sgillen <sgillen@ucsb.edu>
2019-11-18 12:37:05 -0800

    moved register_envs() into envs/__init___, created symetric pendulum env
    Former-commit-id: 7d3c99d0bbb14689f05578729f4747d6a5638bed
    Former-commit-id: 41edbd3bf06c989a76fc8506d0f48184bdcd32b6 [formerly 337ebfa6eae6f4fd3013ae5bfa8d482980b33e71] [formerly 7f35ceef38b0ee4480e1944f31d9f592710096a1 [formerly 42aeb5ab6d24fca16a8f6ab5c3b2bda225585e7c]]
    Former-commit-id: 123d9053c3b9451dd0a4dfdcfe4e0d6b8c7242f0 [formerly 3c079c842304d004540ddc5a1e61c4329a69c31a]
    Former-commit-id: 7d822c8dce5eb27b5b6107e0ad76c83067283f0a

--- seagul/envs/classic_control/__init__.py
+++ seagul/envs/classic_control/__init__.py
@@ -1,7 +1,7 @@
 from seagul.envs.classic_control.su_cartpole import SUCartPoleEnv
 from seagul.envs.classic_control.sg_cartpole import SGCartPoleEnv
-
 from seagul.envs.classic_control.su_cartpole_gym import CartPoleEnv
+
 
 #from seagul.envs.classic_control.cartpole_tedrake import SUCartPoleEnv
 
@@ -11,3 +11,5 @@ from seagul.envs.classic_control.su_pendulum import SUPendulumEnv
 from seagul.envs.classic_control.su_cartpole_push import SUCartPolePushEnv
 from seagul.envs.classic_control.acrobot import AcrobotEnv
 from seagul.envs.classic_control.acrobot2 import AcrobotEnv2
+
+from seagul.envs.classic_control.sym_pendulum import PendulumSymEnv
