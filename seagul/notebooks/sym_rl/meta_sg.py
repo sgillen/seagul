2c4e860154b36785292d6ae0f8bd58ee2a4b06be
sgillen <sgillen@ucsb.edu>
2019-12-06 11:21:46 -0800

    minor changes to ppo and ppo_sym

--- seagul/notebooks/sym_rl/meta_sg.py
+++ seagul/notebooks/sym_rl/meta_sg.py
@@ -12,7 +12,7 @@ import torch
 import torch.nn as nn
 
 from seagul.rl.run_utils import run_sg
-from seagul.rl.algos import ppo
+from seagul.rl.algos import ppo, ppo_sym
 from seagul.rl.models import PpoModel, PpoModelActHold
 from seagul.nn import MLP, CategoricalMLP
 
@@ -27,7 +27,7 @@ activation=nn.ReLU
 torch.set_default_dtype(torch.double)
 proc_list = []
 
-for seed in [0]:
+for seed in [4,5,6,7]:
     
     policy = MLP(input_size, output_size, num_layers, layer_size, activation)
     
@@ -47,27 +47,76 @@ for seed in [0]:
     arg_dict = {
         'env_name' : env_name,
         'model' : model,
-        'action_var_schedule' : [.5,.5],
+        'action_var_schedule' : [.5],
         'seed' : seed, #int((time.time() % 1)*1e8),
-        'num_epochs' : 300,
-        'epoch_batch_size': 2048,
+        'num_epochs' : 800,
+        'env_timesteps' : 200,
+        'epoch_batch_size': 1024,
         'gamma' : .95,
-        'p_epochs' : 32,
-        'v_epochs' : 32,
-        'policy_batch_size' : 64,
-        'value_batch_size' : 64
+        'p_epochs' : 10,
+        'v_epochs' : 20,
+        'policy_batch_size' : 2048,
+        'value_batch_size' : 2048,
     }
     
-    run_name = "state_norm" + str(seed)
+    run_name = "sym2_" + str(seed)
 
     
-    run_sg(arg_dict, ppo, run_name, '', "/data/sg_pend2/")
+#    run_sg(arg_dict, ppo, run_name, '', "/data/sym_comp/ppo")
     
-    #p = Process(target=run_sg, args=(arg_dict, ppo, run_name, 'ppo for walker with nn policy, state norm turned on', "/data/linear_ppo/"))
-    #p.start()
-    #proc_list.append(p)
+    p = Process(target=run_sg, args=(arg_dict, ppo_sym, run_name, 'updating value fn only on non mirrored rewards', "/data/sym_comp/"))
+    p.start()
+    proc_list.append(p)
 
     
-#for p in proc_list:
+for p in proc_list:
+   print("joining")
+   p.join()
+
+
+
+# for seed in [4,5,6,7]:
+    
+#     policy = MLP(input_size, output_size, num_layers, layer_size, activation)
+    
+#     # model = PpoModelActHold(
+#     #     policy=policy,
+#     #     value_fn=MLP(input_size, 1, num_layers, layer_size, activation),
+#     #     discrete=False,
+#     #     hold_count = 200
+#     # )
+
+#     model = PpoModel(
+#         policy=policy,
+#         value_fn=MLP(input_size, output_size, num_layers, layer_size, activation),
+#         discrete=False,
+#     )
+
+#     arg_dict = {
+#         'env_name' : env_name,
+#         'model' : model,
+#         'action_var_schedule' : [.5],
+#         'seed' : seed, #int((time.time() % 1)*1e8),
+#         'num_epochs' : 800,
+#         'epoch_batch_size': 1024,
+#         'env_timesteps' : 200,
+#         'gamma' : .95,
+#         'p_epochs' : 32,
+#         'v_epochs' : 32,
+#         'policy_batch_size' : 512,
+#         'value_batch_size' : 512
+#     }
+    
+#     run_name = "ppo" + str(seed)
+
+    
+# #    run_sg(arg_dict, ppo, run_name, '', "/data/sym_comp/ppo")
+    
+#     p = Process(target=run_sg, args=(arg_dict, ppo, run_name, '', "/data/sym_comp/"))
+#     p.start()
+#     proc_list.append(p)
+
+    
+# for p in proc_list:
 #    print("joining")
 #    p.join()
