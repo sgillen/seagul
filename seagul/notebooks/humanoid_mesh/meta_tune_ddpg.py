995208ba91a4dfbd78d55a9f831f2a7b375bebad
sgillen <sgillen@ucsb.edu>
2019-12-19 16:50:31 -0800

    more experiments with humanoid

--- seagul/notebooks/humanoid_mesh/meta_tune_ddpg.py
+++ seagul/notebooks/humanoid_mesh/meta_tune_ddpg.py
@@ -5,11 +5,11 @@ import seagul.envs
 import yaml
 
 
-settings = yaml.load(open('pend_ddpg.yaml'))
-config = settings['pendulum-ddpg']['config']
+#settings = yaml.load(open('pend_td3.yaml'))
+#config = settings['pendulum-ddpg']['config']
 
 
-# config = ddpg.DEFAULT_CONFIG.copy()
+config = ddpg.DEFAULT_CONFIG.copy()
 # config["num_workers"] = 0
 # config["num_envs_per_worker"] = 6
 # config["num_gpus"] = 0
@@ -20,15 +20,22 @@ config = settings['pendulum-ddpg']['config']
 # config["model"] :
 # config['observation_filter'] = 'MeanStdFilter'
 # #config["train_batch_size"] = tune.sample_from(lambda spec: spec.config.sgd_minibatch_size*32)
-config["seed"] = tune.grid_search([0,1,2,3,4,5,6,7])
+#config["seed"] = tune.grid_search([0,1,2,3,4,5,6,7])
 #env_name = "Walker2d-v3"
 #env_name =  "Walker2DBulletEnv-v0"
 #env_name =   "HumanoidBulletEnv-v0"
-env_name  = "Pendulum-v0"
+env_name  = "Humanoid-v2"
 #env_name = "sym_pendulum-v0"#
 #env_name = "dt_pendulum-v0"
 
-config["env"] = env_name  
+config["env"] = env_name
+config["learning_starts"] =  10000
+config["pure_exploration_steps"] =  10000
+#config["evaluation_interval"] = 5
+#config["evaluation_num_episode"] = 10
+config["seed"] = tune.grid_search([0,1,2,3])
+config["num_workers"] = 15
+config["num_gpus"] = 1
 
 #import pprint
 #pprint.pprint(config)
@@ -43,7 +50,7 @@ analysis = tune.run(
     #PPOSymTrainer,
     ddpg.DDPGTrainer,
     config=config,
-    stop={"timesteps_total": 120e6, "episode_reward_mean": -160},
-    local_dir="./data/ddpg_pend2/",
+    stop={"timesteps_total": 5e7, "episode_reward_mean": 6000},
+    local_dir="./data/bench_model/",
     checkpoint_at_end=True,
 )
