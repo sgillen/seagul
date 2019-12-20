3bcd28833076cb7dc1b00e3b610e12f77f60078d
sgillen <sgillen@ucsb.edu>
2019-12-20 15:37:25 -0800

    new experiments with torch sac

--- seagul/notebooks/humanoid_mesh/meta_tune_sac.py
+++ seagul/notebooks/humanoid_mesh/meta_tune_sac.py
@@ -7,10 +7,7 @@ config = sac.DEFAULT_CONFIG.copy()
 env_name  = "Humanoid-v2"
 #env_name = "sym_pendulum-v0"#
 #env_name = "dt_pendulum-v0"
-
 #env_name  = "Pendulum-v0"
-
-
 #env_name =  "Walker2DBulletEnv-v0"
 
 
@@ -19,11 +16,12 @@ config["env"] = env_name
 #config["num_cpus_per_worker"] = 15
 #config["num_envs_per_worker"]  = 15
 #config["train_batch_size"] = 256
-#config["learning_starts"] = 8192
+config["learning_starts"] = 4096
 #config["learning_starts"] =  10000
 #config["pure_exploration_steps"] =  10000
 #config["evaluation_interval"] = 5
 #config["evaluation_num_episode"] = 10
+config["use_state_preprocessor"] = True
 config["seed"] = tune.grid_search([0,1,2,3])
 config["evaluation_interval"] = 1
 config["soft_horizon"] = True
@@ -34,7 +32,7 @@ config["metrics_smoothing_episodes"] = 10
 analysis = tune.run(
     sac.SACTrainer,
     config=config,
-    stop={"timesteps_total": 2e6},
-    local_dir="./data/sac_pend/",
+    stop={"timesteps_total": 2e7},
+    local_dir="./data/sac_hum/",
     checkpoint_at_end=True,
 )
