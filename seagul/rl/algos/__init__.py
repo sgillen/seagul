aed7a4af3e641aa9c6875b25548ad37272c05fdf
sgillen <seangillen1887@gmail.com>
2019-12-26 15:40:18 -0500

    added regression test for ppo (and it works right now)
    Former-commit-id: caba0bbdda63dfc982915daac77ffc715df82da5
    Former-commit-id: 809da63de459aba89e4a3c64a6f5e25a42e0b008 [formerly 37915699483427287f028d99b031fd76f5e0d770] [formerly 6e27d41e2945cea1dd636027a7ba870379d3834b [formerly c56c1692d87eedffe6bc94d632b37e823043e103]]
    Former-commit-id: 30339a83ebe016862f4f5b75a2cfdcb8a0f9f9a0 [formerly 87531f9a6b356a384fc53aa5fd9089285197d134]
    Former-commit-id: fcb7befb7bddc3b872d0fc14d1776395744b7ac5

--- seagul/rl/algos/__init__.py
+++ seagul/rl/algos/__init__.py
@@ -1,3 +1,8 @@
 from seagul.rl.algos.ppo import ppo
 from seagul.rl.algos.ppo_switching import ppo_switch
-from seagul.rl.algos.ppo_sym import ppo_sym
+
+try:
+    from seagul.rl.algos.ppo_sym import ppo_sym
+except ModuleNotFoundError:
+    import warnings
+    warnings.warn("tensorflow not installed, skipping symmetric ppo")
