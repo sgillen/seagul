ba7e8ccfea6bd1468f4ab9136599ac72089815c5
sgillen <seangillen1887@gmail.com>
2020-01-09 19:17:53 -0500

    sac works for pendulum-v0, regression test incoming

--- seagul/rl/algos/sac.py
+++ seagul/rl/algos/sac.py
@@ -5,27 +5,27 @@ import tqdm
 import gym
 import dill
 
-from seagul.rl.common import ReplayBuffer
+from seagul.rl.common import ReplayBuffer, update_var, update_mean
 from seagul.rl.models import RandModel
 
 def sac(
         env_name,
         total_steps,
         model,
-        epoch_batch_size = 4096,
-        replay_batch_size = 2048,
+        epoch_batch_size = 1,
+        replay_batch_size = 64,
         seed=0,
         gamma = .95,
         polyak = .995,
         alpha  =  .2,
-        pol_batch_size=512,
-        val_batch_size=512,
-        q_batch_size  =512,
+        pol_batch_size=64,
+        val_batch_size=64,
+        q_batch_size  =64,
         pol_lr=1e-4,
         val_lr=1e-5,
         q_lr  = 1e-5,
-        exploration_steps = 10000,
-        replay_buf_size = 1000000,
+        exploration_steps = 100,
+        replay_buf_size = int(50000),
         use_gpu=False,
         reward_stop=None,
 ):
@@ -81,154 +81,155 @@ def sac(
     val_preds_hist = []
     val_targ_hist = []
 
-    
     progress_bar = tqdm.tqdm(total=total_steps)
     cur_total_steps = 0
     progress_bar.update(0)
     early_stop = False
 
+    
     while(cur_total_steps < exploration_steps):
-        ep_obs1, ep_acts, ep_rews, ep_obs2, ep_done  = do_rollout(env, random_model)
+        ep_obs1, ep_obs2, ep_acts, ep_rews, ep_done  = do_rollout(env, random_model)
         
         # can def be made more efficient if found to be a bottleneck
-        for obs1,acts,rews,obs2,done in zip(ep_obs1, ep_acts, ep_rews, ep_obs2, ep_done):
-            replay_buf.store(obs1,acts,rews,obs2,done)
+        for obs1,obs2,acts,rews,done in zip(ep_obs1, ep_obs2, ep_acts, ep_rews, ep_done):
+            replay_buf.store(obs1,obs2,acts,rews,done)
             
         ep_steps = ep_rews.shape[0]
         cur_total_steps += ep_steps
 
-    
-    
+
+    progress_bar.update(cur_total_steps)
+
     while (cur_total_steps < total_steps):
-        batch_obs = torch.empty(0)
-        batch_act = torch.empty(0)
-        batch_adv = torch.empty(0)
-        batch_discrew = torch.empty(0)
         cur_batch_steps = 0
 
-        
         # Bail out if we have met out reward threshold
         if len(raw_rew_hist) > 2:
             if raw_rew_hist[-1] >= reward_stop and raw_rew_hist[-2] >= reward_stop:
                 early_stop = True
                 break
-
-
             
         # collect data with the current policy
         # ========================================================================
-        while (cur_batch_steps < epoch_batch_size):
+        while cur_batch_steps < epoch_batch_size:
             
-            ep_obs1, ep_acts, ep_rews, ep_obs2, ep_done  = do_rollout(env, model)
+            ep_obs1, ep_obs2, ep_acts, ep_rews, ep_done  = do_rollout(env, model)
 
             # can def be made more efficient if found to be a bottleneck
-            for obs1,acts,rews,obs2,done in zip(ep_obs1, ep_acts, ep_rews, ep_obs2, ep_done):
-                replay_buf.store(obs1,acts,rews,obs2,done)
+            for obs1, obs2, acts, rews, done in zip(ep_obs1, ep_obs2, ep_acts, ep_rews, ep_done):
+                replay_buf.store(obs1,obs2,acts,rews,done)
 
             ep_steps = ep_rews.shape[0]
             cur_batch_steps += ep_steps
             cur_total_steps += ep_steps
-            
+
         raw_rew_hist.append(torch.sum(ep_rews))
-        # compute targets for Q and V
-        # ========================================================================
         progress_bar.update(cur_batch_steps)
-        replay_obs1, replay_obs2, replay_acts, replay_rews, replay_done = replay_buf.sample_batch(replay_batch_size)
 
-        q_targ = replay_rews + gamma*(1 - replay_done)*target_value_fn(replay_obs2)
-        q_targ = q_targ.detach()
+        #        import ipdb; ipdb.set_trace()
 
-        noise = torch.randn(replay_batch_size, act_size)
-        sample_acts, sample_logp = model.select_action(replay_obs1, noise)
+        for _ in range(ep_steps):
+            # compute targets for Q and V
+            # ========================================================================
+            replay_obs1, replay_obs2, replay_acts, replay_rews, replay_done = replay_buf.sample_batch(replay_batch_size)
 
-        q_input = torch.cat((replay_obs1, sample_acts), dim=1)
-        q_preds = torch.cat((model.q1_fn(q_input), model.q2_fn(q_input)),dim=1)
-        q_min, q_min_idx = torch.min(q_preds, dim=1)
-        
-        v_targ = q_min - alpha*sample_logp
-        v_targ = v_targ.detach()
+            q_targ = replay_rews + gamma*(1 - replay_done)*target_value_fn(replay_obs2)
+            q_targ = q_targ.detach()
 
-        sample_logp_hist.append(sample_logp)
-        q_targ_hist.append(q_targ)
-        q_preds_hist.append(q_preds)
-        q_min_hist.append(q_min)
-        
-        # q_fn update 
-        # ========================================================================
-        training_data = data.TensorDataset(replay_obs1, replay_acts, q_targ)
-        training_generator = data.DataLoader(training_data, batch_size=q_batch_size, shuffle=True)
+            noise = torch.randn(replay_batch_size, act_size)
+            sample_acts, sample_logp = model.select_action(replay_obs1, noise)
 
-        for local_obs, local_act, local_qtarg in training_generator:
-            # Transfer to GPU (if GPU is enabled, else this does nothing)
-            local_obs, local_act, local_qtarg = (local_obs.to(device), local_act.to(device), local_qtarg.to(device))
+            q_in = torch.cat((replay_obs1, sample_acts), dim=1)
+            q_preds = torch.cat((model.q1_fn(q_in), model.q2_fn(q_in)),dim=1)
+            q_min, q_min_idx = torch.min(q_preds, dim=1)
+            q_min = q_min.reshape(-1,1)
 
-            q_in = torch.cat((local_obs, local_act),dim=1)
-            q1_preds = model.q1_fn(q_in)
-            q2_preds = model.q2_fn(q_in)
-            q1_loss = torch.sum(torch.pow(q1_preds - local_qtarg, 2))/(q1_preds.shape[0])
-            q2_loss = torch.sum(torch.pow(q2_preds - local_qtarg, 2))/(q2_preds.shape[0])
+            v_targ = q_min - alpha*sample_logp
+            v_targ = v_targ.detach()
 
-            q1_opt.zero_grad(); q2_opt.zero_grad()
-            q1_loss.backward(); q2_loss.backward()
-            q1_opt.step(); q2_opt.step()
-                
-        # val_fn update 
-        # ========================================================================
-        training_data = data.TensorDataset(replay_obs1, v_targ)
-        training_generator = data.DataLoader(training_data, batch_size=q_batch_size, shuffle=True)
+            sample_logp_hist.append(sample_logp)
+            q_targ_hist.append(q_targ)
+            q_preds_hist.append(q_preds)
+            q_min_hist.append(q_min)
 
-        for local_obs, local_vtarg in training_generator:
-            # Transfer to GPU (if GPU is enabled, else this does nothing)
-            local_obs, local_vtarg = (local_obs.to(device), local_vtarg.to(device))
+            # q_fn update
+            # ========================================================================
+            training_data = data.TensorDataset(replay_obs1, replay_acts, q_targ)
+            training_generator = data.DataLoader(training_data, batch_size=q_batch_size, shuffle=True)
+
+            for local_obs, local_acts, local_qtarg in training_generator:
+                # Transfer to GPU (if GPU is enabled, else this does nothing)
+                local_obs, local_acts, local_qtarg = (local_obs.to(device), local_acts.to(device), local_qtarg.to(device))
+
+                q_in = torch.cat((local_obs, local_acts),dim=1)
+                q1_preds = model.q1_fn(q_in)
+                q2_preds = model.q2_fn(q_in)
+                q1_loss = torch.sum(torch.pow(q1_preds - local_qtarg, 2))/replay_batch_size
+                q2_loss = torch.sum(torch.pow(q2_preds - local_qtarg, 2))/replay_batch_size
+
+                q1_opt.zero_grad(); q2_opt.zero_grad()
+                q1_loss.backward(); q2_loss.backward()
+                q1_opt.step(); q2_opt.step()
+
+            # val_fn update
+            # ========================================================================
+            training_data = data.TensorDataset(replay_obs1, v_targ)
+            training_generator = data.DataLoader(training_data, batch_size=q_batch_size, shuffle=True)
+
+            for local_obs, local_vtarg in training_generator:
+                # Transfer to GPU (if GPU is enabled, else this does nothing)
+                local_obs, local_vtarg = (local_obs.to(device), local_vtarg.to(device))
 
 
-            # predict and calculate loss for the batch
-            val_preds = model.value_fn(local_obs)
-            val_loss =  torch.sum(torch.pow(val_preds - local_vtarg, 2))/(val_preds.shape[0])
-            
-            # do the normal pytorch update
-            val_opt.zero_grad()
-            val_loss.backward()
-            val_opt.step()
+                # predict and calculate loss for the batch
+                val_preds = model.value_fn(local_obs)
+                val_loss = torch.sum(torch.pow(val_preds - local_vtarg, 2))/replay_batch_size
 
-        
-        # policy_fn update
-        # ========================================================================                
-        val_preds_hist.append(val_preds)
-        val_targ_hist.append(v_targ)
-        
-        training_data = data.TensorDataset(replay_obs1, sample_acts, sample_logp)
-        training_generator = data.DataLoader(training_data, batch_size=pol_batch_size, shuffle=True)
+                # do the normal pytorch update
+                val_opt.zero_grad()
+                val_loss.backward()
+                val_opt.step()
 
-        for local_obs in training_generator:
-            # Transfer to GPU (if GPU is enabled, else this does nothing)
-            local_obs = local_obs[0].to(device)
-            
-            noise = torch.randn(pol_batch_size, act_size)
-            local_acts, local_logp = model.select_action(local_obs, noise)
-            
-            q_in = torch.cat((local_obs, local_acts), dim=1)
-            pol_loss =  torch.sum(model.q1_fn(q_in) - alpha*local_logp)
 
-            # do the normal pytorch update
-            pol_opt.zero_grad()
-            pol_loss.backward()
-            pol_opt.step()
-        
-                
-        # Update target value fn with polyak average
-        # ========================================================================                
-        val_loss_hist.append(val_loss.item())
-        pol_loss_hist.append(pol_loss.item())
-        q1_loss_hist.append(q1_loss.item())
-        q2_loss_hist.append(q2_loss.item())
-        
-        val_sd = model.value_fn.state_dict()
-        tar_sd = target_value_fn.state_dict()
-        for t_targ, t in zip(val_sd.values(), tar_sd.values()):
-            t_targ = polyak*t_targ + (1-polyak)*t
+            # policy_fn update
+            # ========================================================================
+            val_preds_hist.append(val_preds)
+            val_targ_hist.append(v_targ)
 
-        target_value_fn.load_state_dict(tar_sd)
+            training_data = data.TensorDataset(replay_obs1)
+            training_generator = data.DataLoader(training_data, batch_size=pol_batch_size, shuffle=True)
+
+            for local_obs in training_generator:
+                # Transfer to GPU (if GPU is enabled, else this does nothing)
+                local_obs = local_obs[0].to(device)
+
+                noise = torch.randn(pol_batch_size, act_size)
+                local_acts, local_logp = model.select_action(local_obs, noise)
+
+                q_in = torch.cat((local_obs, local_acts), dim=1)
+                pol_loss = torch.sum(alpha*local_logp - model.q1_fn(q_in))/replay_batch_size
+
+                if pol_loss < -1e3:
+                    print("nannanan")
+                # do the normal pytorch update
+                pol_opt.zero_grad()
+                pol_loss.backward()
+                pol_opt.step()
+
+
+            # Update target value fn with polyak average
+            # ========================================================================
+            val_loss_hist.append(val_loss.item())
+            pol_loss_hist.append(pol_loss.item())
+            q1_loss_hist.append(q1_loss.item())
+            q2_loss_hist.append(q2_loss.item())
+
+            val_sd = model.value_fn.state_dict()
+            tar_sd = target_value_fn.state_dict()
+            for layer in tar_sd:
+                tar_sd[layer] = polyak * tar_sd[layer] + (1 - polyak) * val_sd[layer]
+
+            target_value_fn.load_state_dict(tar_sd)
 
 
     return (model, raw_rew_hist, locals())
@@ -249,6 +250,10 @@ def do_rollout(env, model):
     
     while not done:
         obs = torch.as_tensor(obs,dtype=dtype).detach()
+
+        if torch.sum(torch.isnan(obs)):
+            print("nanan")
+            
         obs1_list.append(obs.clone())
 
         noise = torch.randn(1, act_size)
@@ -259,14 +264,15 @@ def do_rollout(env, model):
         obs = torch.as_tensor(obs,dtype=dtype).detach()
         
         acts_list.append(torch.as_tensor(act.clone(), dtype=dtype))
-        rews_list.append(rew)
+        rews_list.append(torch.as_tensor(rew, dtype=dtype))
         obs2_list.append(obs.clone())
-        done_list.append(torch.as_tensor(done))
+        #        done_list.append(torch.as_tensor(done))
+        done_list.append(torch.as_tensor(False))
       
     ep_obs1 = torch.stack(obs1_list)
     ep_acts = torch.stack(acts_list)
-    ep_rews = torch.tensor(rews_list, dtype=dtype).reshape(-1,1)
+    ep_rews = torch.stack(rews_list).reshape(-1,1)
     ep_obs2 = torch.stack(obs2_list)
     ep_done = torch.stack(done_list).reshape(-1,1)
 
-    return (ep_obs1, ep_acts, ep_rews, ep_obs2, ep_done)
+    return (ep_obs1, ep_obs2, ep_acts, ep_rews, ep_done)
--- seagul/rl/regression_tests/pend_sac.py
+++ seagul/rl/regression_tests/pend_sac.py
@@ -7,16 +7,12 @@ import matplotlib.pyplot as plt
 from seagul.rl.models import SACModel
 from multiprocessing import Process
 
-
 # TODO for this to work need to convert from numpy dtype to torch
 env_name = "Pendulum-v0"
 # env = gym.make(env_name)
 # dtype = env.action_space.sample().dtype
 
-
 torch.set_default_dtype(torch.float32)
-
-
 
 input_size = 3
 output_size = 1
@@ -24,43 +20,37 @@ layer_size = 64
 num_layers = 2
 activation = nn.ReLU
 
-policy = MLP(input_size, output_size*2, num_layers, layer_size, activation)
+policy = MLP(input_size, output_size * 2, num_layers, layer_size, activation)
 
 # Do I need to do weight sharing here?
 value_fn = MLP(input_size, 1, num_layers, layer_size, activation)
 q1_fn = MLP(input_size + output_size, 1, num_layers, layer_size, activation)
 q2_fn = MLP(input_size + output_size, 1, num_layers, layer_size, activation)
 
-model = SACModel(policy, value_fn, q1_fn, q2_fn, 5)
+model = SACModel(policy, value_fn, q1_fn, q2_fn, 1)
 
-def run_and_test(arg_dict,seed):
-    t_model, rewards, var_dict = ppo("Pendulum-v0", 200*2048, model, seed=seed, **arg_dict)
+
+def run_and_test(arg_dict, seed):
+    t_model, rewards, var_dict = sac("Pendulum-v0", 200 * 2048, model, seed=seed, **arg_dict)
 
     if (var_dict['early_stop']):
         print("seed", seed, "achieved 200 reward in ", len(rewards), "steps")
     #        print("Rewards were", rewards)
 
     else:
-        print("Error: seed:" , seed, "failed")
+        print("Error: seed:", seed, "failed")
         print("Rewards were", rewards)
 
     return
-    
+
 
 # Define our hyper parameters
 arg_dict = {
-    'epoch_batch_size' : 2048,  # how many steps we want to use before we update our gradients
-    'replay_batch_size' : 2048,
-    'reward_stop' : -200,
-    'pol_batch_size' : 512,
-    'val_batch_size' : 512,
-    'pol_lr' : 1e-2,
-    'val_lr' : 1e-2,
+    'reward_stop': -200,
+    'seed': 2,
 }
 
-
-
-model, rews = sac(env_name, 2e6, model,**arg_dict)
+model, rews, var_dict = sac(env_name, 10000, model, **arg_dict)
 print(rews)
 
 # proc_list = []
@@ -73,3 +63,5 @@ print(rews)
 # for p in proc_list:
 #     print("joining")
 #     p.join()
+
+globals().update(var_dict)
