e493f4be8b3729721c2abc197e244977d2880378
sgillen <seangillen1887@gmail.com>
2020-01-16 16:01:18 -0800

    slightly improved regression tests
    Former-commit-id: 790abf8162d2d5f5e18c8a3706c9a8e2c501df57
    Former-commit-id: 804e1febb5718f30d5da3b6c0bcd3fa0e38dd25a [formerly 36a085e3614fb705cbc8f60b49997a54485c1abb] [formerly 057fd603c8d0ad4ef31351a4369e19dd8ad775e5 [formerly ff6ecea856aeffb13a72d91211e47af9224778fa]]
    Former-commit-id: 21d597b2e634ff59264bb794685eac4bedda4b1a [formerly 6fa0509f24ddf0c2b4f88d88f5c60cfc48b87bba]
    Former-commit-id: 6203e34452e7bc9aa9f38967904424e91e136eca

--- seagul/rl/regression_tests/pend_sac.py
+++ seagul/rl/regression_tests/pend_sac.py
@@ -1,12 +1,24 @@
-import gym
 import torch.nn as nn
 from seagul.rl.algos.sac import sac
 from seagul.nn import MLP
-import torch
 from seagul.rl.models import SACModel
-from multiprocessing import Process
+from multiprocessing import Process, Manager
+import sys
 
-env_name = "Pendulum-v0"
+
+"""
+Basic smoke test for SAC. This file contains an arg_dict that contains hyper parameters known to work with 
+seagul's implementation of SAC. You can also run this script directly, which will check if the algorithm 
+suceeds across 4 random seeds
+
+Example:
+
+from seagul.rl.regression_tests.pend_sac import arg_dict
+from seagul.rl.algos import sac 
+t_model, rewards, var_dict = sac(**arg_dict)  # Should get to -200 reward
+
+"""
+
 input_size = 3
 output_size = 1
 layer_size = 64
@@ -19,26 +31,46 @@ q1_fn = MLP(input_size + output_size, 1, num_layers, layer_size, activation)
 q2_fn = MLP(input_size + output_size, 1, num_layers, layer_size, activation)
 model = SACModel(policy, value_fn, q1_fn, q2_fn, 1)
 
-def run_and_test(arg_dict, seed):
-    t_model, rewards, var_dict = sac("Pendulum-v0", 20000, model, seed=seed, **arg_dict)
+def run_and_test(arg_dict, retval):
 
+    t_model, rewards, var_dict = sac(**arg_dict)
+
+    seed = arg_dict["seed"]
     if var_dict["early_stop"]:
         print("seed", seed, "achieved 200 reward in ", len(rewards), "steps")
+        retval[seed] = True
     else:
         print("Error: seed:", seed, "failed")
         print("Rewards were", rewards)
-    return
+        retval[seed] = False
 
 
 # Define our hyper parameters
-arg_dict = {"reward_stop": -200}
+arg_dict = {
+    "env_name" : "Pendulum-v0",
+    "model" : model,
+    "total_steps" : 20000,
+    "reward_stop" : -200
+    }
 
-proc_list = []
-for seed in [0, 1, 2, 3]:
-    p = Process(target=run_and_test, args=(arg_dict, seed))
-    p.start()
-    proc_list.append(p)
+if __name__ == "__main__" :
+    #orig = sys.stdout
+    #sys.stdout = open("/dev/null")
 
-for p in proc_list:
-    print("joining")
-    p.join()
+    proc_list = []
+    manager = Manager()
+    ret_dict = manager.dict()
+
+    for seed in [0, 1, 2, 3]:
+        arg_dict["seed"] = seed
+        p = Process(target=run_and_test, args=(arg_dict,ret_dict))
+        p.start()
+        proc_list.append(p)
+
+    for p in proc_list:
+        print("joining")
+        p.join()
+
+
+    #sys.stdout = orig
+    print(ret_dict)
