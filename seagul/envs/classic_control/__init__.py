d351b740da52176ff82ba83e51a1f3519fc2d37c
sgillen <sgillen@ucsb.edu>
2020-02-26 11:32:54 -0800

    more work on acrobot stuff
    Former-commit-id: fdc4af2bad4b9342e3edc17e37c09a8280a1c934
    Former-commit-id: c0402bf9d5394740fa1de2daf3c5551065cabf2d [formerly 974b77c48e48deb4ee0ed8988e61d1a669a2f99d] [formerly 14b8a679c7d66672962506e4f2f4a2d3c5d143ef [formerly ca9957a7789c425787259ce156649efc9a99ead7]]
    Former-commit-id: 4cb44d5afdcd1f193bc500a7d02c5c7038ccc29e [formerly 1d42ee7923a696fb31b9778f8d14da14a10b14cd]
    Former-commit-id: 7fe9495827d29a5af7aba3c0538da601ebb20327

--- seagul/envs/classic_control/__init__.py
+++ seagul/envs/classic_control/__init__.py
@@ -8,7 +8,7 @@ from seagul.envs.classic_control.su_cartpole_discrete import SUCartPoleDiscEnv
 
 from seagul.envs.classic_control.su_pendulum import SUPendulumEnv
 from seagul.envs.classic_control.su_cartpole_push import SUCartPolePushEnv
-from seagul.envs.classic_control.acrobot import AcrobotEnv
+from seagul.envs.classic_control.acrobot import SGAcroEnv
 from seagul.envs.classic_control.acrobot2 import AcrobotEnv2
 
 from seagul.envs.classic_control.sym_pendulum import PendulumSymEnv
