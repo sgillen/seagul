f763a0d26c259a9ebc918ece6c82f4f6db889c2d
sgillen <sgillen@ucsb.edu>
2020-02-29 00:49:45 -0800

    switching bugfix

--- seagul/notebooks/switching/meta_run_switched.py
+++ seagul/notebooks/switching/meta_run_switched.py
@@ -1,8 +1,6 @@
 import gym
 import seagul.envs
 
-env_name = "su_acro_drake-v0"
-env = gym.make(env_name)
 
 from seagul.rl.run_utils import run_sg, run_and_save_bs
 from seagul.rl.algos import ppo, ppo_switch
@@ -18,85 +16,105 @@ from numpy import pi
 from multiprocessing import Process
 import time
 
+trial_num = input("what trial is this?")
+
 # init policy, value fn
 input_size = 4
 output_size = 1
-layer_size = 24
+layer_size = 32
 num_layers = 2
 activation = nn.ReLU
 proc_list = []
 
-torch.set_num_threads(1)
+m1 = 1
+m2 = 1
+l1 = 1
+l2 = 2
+lc1 = .5
+lc2 = 1
+I1 = .083
+I2 = .33
+g = 9.8
 
-for seed in [1,2,3,4]:
-    env_name = "su_acro_drake-v0"
-    env = gym.make(env_name)
+k = np.array([[-787.27034288, -321.29901324, -342.50908801, -158.94341213]])
 
-    run_name = "heavy_switch"
+def control(q):
+    gs = np.array([pi / 2, 0, 0, 0])
+    return -k.dot(q - gs)
 
-    def control(q):
-        k = np.array([[1316.85000612, 555.41763935, 570.32667002, 272.57631536]], dtype=np.float32)
-        # k = np.array([[278.44223126, 112.29125985, 119.72457377,  56.82824017]])
-        gs = np.array([pi, 0, 0, 0], dtype=np.float32)
-        # return 0
-        return (-k.dot(gs - np.asarray(q)))
+def reward_fn(s, a):
+    reward = np.sin(s[0]) + 2 * np.sin(s[0] + s[1])
+    return reward, False
+
+max_torque = 25
+
+env_config = {"init_state": [0, 0, 0, 0],
+              "max_torque": max_torque,
+              "init_state_weights": [0, 0, 0, 0],
+              "max_t": 2.5,
+              "m2": m2,
+              "m1": m1,
+              "l1": l1,
+              "lc1": lc1,
+              "lc2": lc2,
+              "i1": I1,
+              "i2": I2,
+              "reward_fn": reward_fn,
+              "act_hold": 1
+              }
+
+
+for seed in np.random.randint(0, 2**32,4):
+
+    env_name = "su_acrobot-v0"
+    env = gym.make(env_name, **env_config)
+
+    run_name = "newnew"
+
+
 
     model = SwitchedPPOModelActHold(
         # policy = MLP(input_size, output_size, num_layers, layer_size, activation),
-        policy=torch.load("warm/ppo2_warm_polh"),
-        value_fn=torch.load("warm/ppo2_warm_valh"),
-        # MLP(input_size, 1, num_layers, layer_size, activation),
-        gate_fn=torch.load("warm/gate_fn_ppo2_nz128"),
+        policy=MLP(input_size, output_size, layer_size, num_layers),
+        value_fn=MLP(input_size, output_size, layer_size, num_layers),
+        gate_fn=torch.load("warm/gate_fn_good"),
         nominal_policy=control,
         hold_count=0,
     )
-    
-    def reward_fn(ns, act):
-        return 1e-2*-(np.cos(ns[0]) + np.cos(ns[0] + ns[1]))
 
-    env_config = {
-        "max_torque" : 700,
-        "init_state" : [0.0, 0.0, 0.0, 0.0],
-        "init_state_weights" : np.array([1, 1, 0, 0]),
-        "dt" : .01,
-        "max_t" : 5,
-        "act_hold" : 20,
-        "fixed_step" : True,
-        "int_accuracy" : .01,
-        "reward_fn" : reward_fn
-    }
 
-    arg_dict = {
+    alg_config = {
         "env_name": env_name,
         "model": model,
-        "total_steps": 1e6,
+        "total_steps": 5e4,
         "epoch_batch_size": 2048,
-        "act_var_schedule": [1, 1],
+        "act_var_schedule": [3],
         "gate_var_schedule": [0.1, 0.1],
         "gamma": 1,
         "seed": seed,
         "reward_stop": 1500,
-        "pol_epochs": 10,
+        "pol_epochs": 300,
         "val_epochs": 10,
         "env_config": env_config
     }
 
-    p = Process(
-        target=run_sg,
-        args=(
-            arg_dict,
-            ppo_switch,
-            run_name,
-            "heavier second link",
-            "/data2/ht/",
-        ),
-    )
-    p.start()
-    proc_list.append(p)
+    run_sg(alg_config, ppo_switch, run_name, "debug" , "/data/debug")
 
-for p in proc_list:
-    print("joining")
-    p.join()
-
+#     p = Process(
+#         target=run_sg,
+#         args=(
+#             alg_config,
+#             ppo_switch,
+#             run_name,
+#             "testing new gate",
+#             "/data4/trial" + trial_num + "/",
+#         ),
+#     )
+#     p.start()
+#     proc_list.append(p)
+#
+# for p in proc_list:
+#     print("joining")
+#     p.join()
 
 print("finished run ", run_name)
