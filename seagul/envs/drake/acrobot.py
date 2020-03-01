4555ab5ab40fead884dcfb71a5f4187de5fc488b
sgillen <sgillen@ucsb.edu>
2020-03-01 13:51:46 -0800

    fixed drake env
    Former-commit-id: 8739fe254253a459991119ab28b13508298f64cf
    Former-commit-id: 18e01f0d3c4ac9b8f6ed4d3d1a906f974ea40f77 [formerly d5e0e8c7047b6da1c41524e557e62b7295628341] [formerly 648dacc3d76a9fe75731cf5f075dc2c9be0f3d58 [formerly 13faf9c1c1c40310163335aef52f4c0f694a9840]]
    Former-commit-id: 4dd4c7221018b1b6edf05ecc7a5adaaf2f9b6b09 [formerly c86d1e8a2e2442206c6abb8753fa9b7a4e05622e]
    Former-commit-id: 8d04d30fe532f5c2f4bfac99ee670345b07af90b

--- seagul/envs/drake/acrobot.py
+++ seagul/envs/drake/acrobot.py
@@ -159,7 +159,7 @@ class DrakeAcroEnv(core.Env):
 
         init_state = self.InitialState().CopyToVector()
 
-        init_state += np.random.random(4)*np.random.random(4)(self.state_weights*2) - self.state_weights
+        init_state += np.random.random(4)*(self.init_state_weights*2) - self.init_state_weights
 
         if init_vec is not None:
             init_state[0] = init_vec[0]
