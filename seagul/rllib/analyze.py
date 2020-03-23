c405ef2923bec33458ac916ba3dab11f9ab8e239
ninafiona <ninafiona@gmx.de>
2020-03-23 03:14:08 -0700

    png temp

--- seagul/rllib/analyze.py
+++ seagul/rllib/analyze.py
@@ -6,19 +6,18 @@ import analysis_functions
 checkpoint = "3544" # only needed for rendering
 # res_dir = ["./Results/HalfCheetah/PPO/FCN_1/PPO_HalfCheetahBulletEnv-v0_6dee9f88_2020-03-01_19-03-4581ly820s/"] # 180
 # res_dir = ["./data/HalfCheetahBulletEnv-v0/PPO/"] # 10
-res_dir = ["./rllib_with_rbf/data/Pendulum-v0/SAC/"] # 31
+res_dir = ["./data/Pendulum-v0/SAC/"] # 31
 
 entries = analysis_functions.outputs_to_df(res_dir, cutoff=-1)
 analysis_functions.plot_outputs(entries)
 env, alg = analysis_functions.get_params(res_dir)  
 plt.title("Environment: " + env + ",  Algorithm: " + alg)
 
-plot = True
+plot = False
 if plot:
     plt.show()
-    x=1
 else:
     name = env + "_" + alg
-    plt.savefig("./Results/" + name + ".png")
+    plt.savefig("./data/" + name + ".png")
 
 analysis_functions.render(checkpoint, res_dir[0])
