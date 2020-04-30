47f20850a7a8554c7aa17fbb8d873b201d66f938
sgillen <sgillen@ucsb.edu>
2020-04-29 17:56:27 -0700

    state norms working in sac

--- seagul/rl/tests/pend_sac.py
+++ seagul/rl/tests/pend_sac.py
@@ -66,8 +66,8 @@ if __name__ == "__main__" :
         "use_gpu": False,
     }
     #
-    # arg_dict["seed"] = 0
-    # run_and_test(arg_dict)
+    #arg_dict["seed"] = 0
+    #run_and_test(arg_dict)
 
     proc_list = []
 
