350f1a5d96d35253b1bb72a548e1a92b8d28baf9
sgillen <sgillen@ucsb.edu>
2020-05-06 23:11:58 -0700

    bball stuff training with stable, per run reward and done functions

--- seagul/notebooks/bball/train_bball_stable.py
+++ seagul/notebooks/bball/train_bball_stable.py
@@ -1,12 +1,13 @@
 import gym
 import numpy as np
+from numpy import pi
 from stable_baselines.common.policies import MlpPolicy
 from stable_baselines import PPO2
 from multiprocessing import Process
 from stable_baselines.common import make_vec_env
 import os
 import time
-import seagul.envs
+from seagul.envs.matlab.bball_env import BBallEnv
 
 num_steps = int(5e5)
 base_dir = "data2/"
@@ -19,12 +20,19 @@ if base_ok == "n":
     exit()
 
 def run_stable(num_steps, save_dir):
+    def reward_fn(state, action):
+        return state[3]
 
+    def done_criteria(state):
+        return state[3] < (.3*np.cos(state[0]) + .3*np.cos(state[0] + state[1]) )
 
+    env_config = {
+        'init_state' : (-pi / 4, 3 * pi / 4, 0.025, .5, 0, 0, 0, 0),
+        'reward_fn' : reward_fn,
+        'done_criteria' : done_criteria
+    }
 
-
-
-    env = make_vec_env('bball-v0', n_envs=1, monitor_dir=save_dir)
+    env = make_vec_env(BBallEnv, n_envs=1, monitor_dir=save_dir, env_kwargs=env_config)
 
     model = PPO2(MlpPolicy,
                  env,
