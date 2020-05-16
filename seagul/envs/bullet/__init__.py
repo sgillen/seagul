b3515971b7fc46096f983509020df3d04c2d6bb8
sgillen <sgillen@ucsb.edu>
2020-05-15 17:14:08 -0700

    remove outdated bullet race car env
    Former-commit-id: 3b930bf34e3265d4e72d9d95a5bb920d7e02e4c7
    Former-commit-id: f5415c2c7da1b56ea73ab14ee9aaf2ad69f14f5e [formerly dc9b745efb817d70c4e5f818a75feb8b9726a5ee] [formerly a3cdf13a0a84fa8d8bf3a410b7cd9af3a84c66fc [formerly 6d908560e391390beb8916ddef9ade6186e4ce33]]
    Former-commit-id: 3f012f1104049891d84c51a01bde0e8b701f0498 [formerly 06f64ae0054aaeaaf546c6b13508a183e6ef612f]
    Former-commit-id: e0c04a2f05be0fcad84d0fd2a01cf765700ffff5

--- seagul/envs/bullet/__init__.py
+++ seagul/envs/bullet/__init__.py
@@ -1,4 +1,4 @@
 # from seagul.envs.bullet.walker2d_fl import Walker2DFiveLink
-from seagul.envs.bullet.bullet_car import RacecarGymEnv_v1
-from seagul.envs.bullet.bullet_car_ast import RacecarGymEnvAst_v1
+#from seagul.envs.bullet.bullet_car import RacecarGymEnv_v1
+#from seagul.envs.bullet.bullet_car_ast import RacecarGymEnvAst_v1
 from seagul.envs.bullet.walker import PBMJWalker2dEnv
