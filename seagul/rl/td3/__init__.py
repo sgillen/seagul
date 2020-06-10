1a079e933fef18f56d5840370ddbd912bcd75ded
sgillen <sgillen@ucsb.edu>
2020-06-09 19:36:11 -0700

    ddpg works

--- seagul/rl/td3/__init__.py
+++ seagul/rl/td3/__init__.py
@@ -1,2 +1,3 @@
 from seagul.rl.td3.td3 import td3
+from seagul.rl.td3.ddpg import ddpg
 from seagul.rl.td3.models import TD3Model
--- seagul/rl/td3/ddpg.py
+++ seagul/rl/td3/ddpg.py
@@ -1,24 +1,24 @@
 from seagul.rl.common import ReplayBuffer, RandModel, make_schedule, update_target_fn
 import numpy as np
-
+import copy
 import gym
 import dill
 import tqdm.auto as tqdm
 import torch
 
-def td3(
+
+def ddpg(
         env_name,
         train_steps,
         model,
         env_max_steps=0,
         min_steps_per_update=1,
-        iters_per_update=200,
+        iters_per_update=float('inf'),
         replay_batch_size=64,
         seed=0,
         act_std_schedule=(.1,),
         gamma=0.95,
         polyak=0.995,
-        sgd_batch_size=64,
         sgd_lr=3e-4,
         exploration_steps=1000,
         replay_buf_size=int(100000),
@@ -44,18 +44,13 @@ def td3(
     np.random.seed(seed)
 
     random_model = RandModel(model.act_limit, act_size)
+    target_model = copy.deepcopy(model)
     replay_buf = ReplayBuffer(obs_size, act_size, replay_buf_size)
-    target_q1_fn = dill.loads(dill.dumps(model.q1_fn))
-    target_q2_fn = dill.loads(dill.dumps(model.q2_fn))
-    target_policy = dill.loads(dill.dumps(model.policy))
 
-    for param in target_q1_fn.parameters():
+    for param in target_model.q1_fn.parameters():
         param.requires_grad = False
 
-    for param in target_q2_fn.parameters():
-        param.requires_grad = False
-
-    for param in target_policy.parameters():
+    for param in target_model.policy.parameters():
         param.requires_grad = False
 
     act_std_lookup = make_schedule(act_std_schedule, train_steps)
@@ -63,7 +58,6 @@ def td3(
 
     pol_opt = torch.optim.Adam(model.policy.parameters(), lr=sgd_lr)
     q1_opt = torch.optim.Adam(model.q1_fn.parameters(), lr=sgd_lr)
-    q2_opt = torch.optim.Adam(model.q2_fn.parameters(), lr=sgd_lr)
 
     progress_bar = tqdm.tqdm(total=train_steps)
     cur_total_steps = 0
@@ -93,7 +87,6 @@ def td3(
 
         # Bail out if we have met out reward threshold
         if len(raw_rew_hist) > 2 and reward_stop:
-            print(raw_rew_hist[-1])
             if raw_rew_hist[-1] >= reward_stop and raw_rew_hist[-2] >= reward_stop:
                 early_stop = True
                 break
@@ -116,52 +109,40 @@ def td3(
         # ========================================================================
         for _ in range(min(int(ep_steps), iters_per_update)):
 
-            # Compute target Q
             replay_obs1, replay_obs2, replay_acts, replay_rews, replay_done = replay_buf.sample_batch(replay_batch_size)
+#            print(raw_rew_hist[-1])
 
+            # Compute target Q
             with torch.no_grad():
-                acts_from_target = target_policy(replay_obs2)
-                q_in = torch.cat((replay_obs2, acts_from_target), dim=1)
-                q_targ = replay_rews + gamma*(1 - replay_done)*target_q1_fn(q_in)
-
-            num_mbatch = int(replay_batch_size / sgd_batch_size)
+                noise = torch.randn(replay_batch_size, act_size) * act_std
+                target_acts, _ = target_model.select_action(replay_obs2, noise)
+                q_in = torch.cat((replay_obs2, target_acts), dim=1)
+                q_targ = replay_rews + gamma * (1 - replay_done) * target_model.q1_fn(q_in)
 
             # q_fn update
             # ========================================================================
-            for i in range(num_mbatch):
-                cur_sample = i * sgd_batch_size
+            q_in = torch.cat((replay_obs1, replay_acts), dim=1)
+            q_loss = ((model.q1_fn(q_in) - q_targ)**2).mean()
+#            print('q_loss:', q_loss)
 
-                q_in_local = torch.cat((replay_obs1[cur_sample:cur_sample + sgd_batch_size], replay_acts[cur_sample:cur_sample + sgd_batch_size]), dim=1)
-                local_qtarg = q_targ[cur_sample:cur_sample + sgd_batch_size]
-
-                q1_loss = ((model.q1_fn(q_in_local) - local_qtarg)**2).mean()
-
-                #q2_preds = model.q2_fn(q_in)
-                #q2_loss = (q2_preds - q_targ[cur_sample:cur_sample + sgd_batch_size]**2).mean()
-                q_loss = q1_loss# + q2_loss
-
-                q1_opt.zero_grad()
-                #q2_opt.zero_grad()
-                q_loss.backward()
-                q1_opt.step()
-                #q2_opt.step()
+            q1_opt.zero_grad()
+            q_loss.backward()
+            q1_opt.step()
 
             # policy_fn update
             # ========================================================================
             for param in model.q1_fn.parameters():
                 param.requires_grad = False
 
-            for i in range(num_mbatch):
-                cur_sample = i * sgd_batch_size
-                local_obs = replay_obs1[cur_sample:cur_sample + sgd_batch_size]
-                local_acts = model.policy(local_obs)
-                q_in = torch.cat((local_obs, local_acts), dim=1)
+            local_acts = model.policy(replay_obs1)
+            q_in = torch.cat((replay_obs1, local_acts), dim=1)
 
-                pol_loss = -(model.q1_fn(q_in).mean())
+            pol_loss = -(model.q1_fn(q_in).mean())
+#            print('pol_loss:', pol_loss)
 
-                pol_opt.zero_grad()
-                pol_loss.backward()
-                pol_opt.step()
+            pol_opt.zero_grad()
+            pol_loss.backward()
+            pol_opt.step()
 
             for param in model.q1_fn.parameters():
                 param.requires_grad = True
@@ -169,12 +150,10 @@ def td3(
             # Update target value fn with polyak average
             # ========================================================================
             pol_loss_hist.append(pol_loss.item())
-            q1_loss_hist.append(q1_loss.item())
-            #q2_loss_hist.append(q2_loss.item())
+            q1_loss_hist.append(q_loss.item())
 
-            target_q1_fn = update_target_fn(model.q1_fn, target_q1_fn, polyak)
-            target_q2_fn = update_target_fn(model.q2_fn, target_q2_fn, polyak)
-            target_policy = update_target_fn(model.policy, target_policy, polyak)
+            target_model.q1_fn = update_target_fn(model.q1_fn, target_model.q1_fn, polyak)
+            target_model.policy = update_target_fn(model.policy, target_model.policy, polyak)
             act_std = act_std_lookup(cur_total_steps)
 
     return model, raw_rew_hist, locals()
