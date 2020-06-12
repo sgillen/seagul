ce713454ddb45109bd5a25188ce6de4288110192
sgillen <sgillen@ucsb.edu>
2020-06-12 13:55:31 -0700

    updated update std to use std (and no variance...)
    Former-commit-id: f919de2e55bbbc7dc3c08f82c3028432e6c56982
    Former-commit-id: 36fb250872e08d9b0900cfef67c542ea52dfe03b [formerly 6bbe721721b3a74d5ce7bc048f34729885413e40] [formerly 13dac49cb3149e879568c9bd8f6db4b192874b20 [formerly e4f3f31ca4ea3047033d261ef97cc0f5ef1fc5b9]]
    Former-commit-id: 14d0346244552c5df40f2d957c793e46f8da34ba [formerly 80fe0772877611056db18fbb64590b155a2e6685]
    Former-commit-id: ca18e3ed74e7ee1ee76b196bd1708103dd3c0540

--- seagul/rl/ppo/__init__.py
+++ seagul/rl/ppo/__init__.py
@@ -1,5 +1,5 @@
 from seagul.rl.ppo.ppo2 import ppo
 from seagul.rl.ppo.ppo2_switch import ppo_switch
-from seagul.rl.ppo.ppo_dim import ppo_dim
-from seagul.rl.ppo.ppo_visit import ppo_visit
+from seagul.rl.ppo.ppo2_dim import ppo_dim
+from seagul.rl.ppo.ppo2_visit import ppo_visit
 from seagul.rl.ppo.models import PPOModel, PPOModelActHold, SwitchedPPOModel, SwitchedPPOModelActHold
