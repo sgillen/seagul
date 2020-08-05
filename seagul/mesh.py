126dbfcd06e567130dd1a420c70be8265fdb1566
sgillen <sgillen@ucsb.edu>
2020-08-05 11:43:32 -0700

    cleanup for box dim

--- seagul/mesh.py
+++ seagul/mesh.py
@@ -189,48 +189,43 @@ def create_box_mesh(data, d, initial_mesh=None):
     return mesh
 
 
-
-def mesh_dim(data, init_d=1e-2):
+def mesh_dim(data, scaling_factor=1.5, init_d=1e-2, upper_size_ratio=4/5, lower_size_ratio=0.0, d_limit=1e-9):
     """
     Args:
-        data - any np array or torch thing
-        init_d - initial mesh size
+        data - any array like, represents the trajectory you want to compute the dimension of
+        scaling factors - float indicating how much to scale d by for every new mesh
+        init_d - float, initial box size
+        upper_size_ratio - upper_size_ratio*data.shape[0] determines what size of mesh to stop at when finding the upper bound of the curve.
+        lower_size_ratio - lower_size_ratio*data.shape[0] determines what size of mesh to stop at when finding the lower bound of the curve. Usually best to leave at 0.
+        d_limit - smallest d value to allow when seeking the upper_size bound
 
     Returns:
-        mesh_dim, mesh_sizes, d_vals
-
+        mdim: linear fit to the log(mesh) log(d) data, intentional underestimate of the meshing dimensions
+        cdim: the conservative mesh dimension, that is the largest slope from the log log data, an intentional overestimate of the
+        mesh_sizes: sizes of each mesh created during the computation
+        d_vals: box sizes used to create each mesh during the computation
     """
-    scale_factor = 1.5
-    mesh_size_upper = 4/5*data.shape[0]
 
-    mesh = create_box_mesh(data, init_d)
+    mesh_size_upper = np.round(upper_size_ratio * data.shape[0])
+    mesh_size_lower = np.round(np.max((1.0, lower_size_ratio * data.shape[0])))
+    d = init_d
+
+    mesh = create_box_mesh(data, d)
     mesh_sizes = [len(mesh)]
-    d_vals = [init_d]
-    if len(mesh) > mesh_size_upper:
-        #print("Warning initial d for mesh too large! auto adjusting")
+    d_vals = [d]
 
-        d = init_d/scale_factor
-        while True:
-            mesh = create_box_mesh(data, d)
-            mesh_sizes.insert(0, len(mesh))
-            d_vals.insert(0, d)
+    while mesh_sizes[0] < mesh_size_upper and d > d_limit:
+        d /= scaling_factor
+        mesh = create_box_mesh(data, d)
+        mesh_sizes.insert(0, len(mesh))
+        d_vals.insert(0, d)
 
-            d = d/scale_factor
-
-            if mesh_sizes[0] > mesh_size_upper or d < 1e-9:
-                #print("d found: ", d, mesh_sizes, data.shape[0])
-                break
-
-    d = init_d*scale_factor
-    while True:
+    d = init_d
+    while mesh_sizes[-1] > mesh_size_lower and d > d_limit:
+        d = d * scaling_factor
         mesh = create_box_mesh(data, d)
         mesh_sizes.append(len(mesh))
         d_vals.append(d)
-
-        if mesh_sizes[-1] == 1:
-            break
-
-        d = d * scale_factor
 
     for i, m in enumerate(mesh_sizes):
         if m < mesh_size_upper:
@@ -244,7 +239,7 @@ def mesh_dim(data, init_d=1e-2):
     def f(x, m, b):
         return m * x + b
 
-    popt, pcov = opt.curve_fit(f, xdata, -ydata)
+    popt, pcov = opt.curve_fit(f, xdata, ydata)
 
     # find the largest slope
     min_slope = 0
@@ -253,41 +248,7 @@ def mesh_dim(data, init_d=1e-2):
         if slope < min_slope:
             min_slope = slope
 
-    return popt[0], -min_slope, mesh_sizes, d_vals
-
-
-def conservative_mesh_dim(data, init_d=1e-3):
-    """
-    Args:
-        data - any np array or torch thing
-        init_d - initial mesh size
-
-    Returns:
-        mesh_dim, mesh_sizes, d_vals
-
-    """
-    mesh_sizes = []
-    d_vals = []
-    d = init_d
-    while True:
-        mesh = create_box_mesh(data, d)
-        mesh_sizes.append(len(mesh))
-        d_vals.append(d)
-
-        if mesh_sizes[-1] == 1:
-            break
-
-        d = d * 2
-
-
-
-    min_slope = 0
-    for i in range(len(mesh_sizes)-1):
-        slope = - mesh_sizes[i] - mesh_sizes[i+1]/(d_vals[i+1] - d_vals[i])
-        if slope < min_slope:
-            min_slope = slope
-
-    return -min_slope, mesh_sizes, d_vals
+    return -popt[0], -min_slope, mesh_sizes, d_vals
 
 
 def power_var(X, l, ord):
@@ -298,10 +259,6 @@ def power_var(X, l, ord):
         
     return 1 / (2 * len(X) - l) * np.sum(norms)
 
-# def power_var(X, l, ord):
-#     # Implements the power variation, used for the variation fractal dimension
-#     return 1 / (2 * len(X) - l) * np.sum(np.linalg.norm(X[l:] - X[:-l],ord=ord))
-
 
 def variation_dim(X, order=1):
     # Implements the order p variation fractal dimension from https://arxiv.org/pdf/1101.1444.pdf (eq 18)
@@ -309,31 +266,256 @@ def variation_dim(X, order=1):
     return 2 - 1/(order*np.log(2))*(np.log(power_var(X, 2, order)) - np.log(power_var(X, 1, order)))
 
 
+# if __name__ == "__main__":
+#     import numpy as np
+#     import time
+#     import matplotlib.pyplot as plt
+#
+#     data_size = 10000
+#     meshes = []
+#
+#     dim = 17
+#     coefs = np.random.random((dim,))
+#     start = np.zeros((dim,))
+#     stop = np.ones((dim)) * 10
+#
+#     x = np.linspace(start, stop, 100)
+#     noise = np.random.random(x.shape)
+#     data = coefs * (x + noise)
+#
+#     #X = np.((int(data_size), 10))
+#     start = time.time()
+#     m, c, mesh_sizes, d_vals = mesh_dim(data, .01)
+#
+#     xdata = np.log2(d_vals)
+#     ydata = np.log2(mesh_sizes)
+#
+#     plt.plot(d_vals, mesh_sizes, 'x-')
+#     plt.show()
+#     plt.plot(xdata, ydata, 'x-')
+#     plt.show()
+
+
 if __name__ == "__main__":
-    import numpy as np
-    import time
+    from seagul.rl.ars.ars_pipe import ars
+    from seagul.nn import MLP
+    import torch
     import matplotlib.pyplot as plt
+    from seagul.mesh import create_mesh, variation_dim, mesh_dim, create_box_mesh
+    import time
+    import copy
+    import gym
+    import torch
 
-    data_size = 10000
-    meshes = []
+    import xarray as xr
+    import numpy as np
+    import pandas as pd
 
-    dim = 17
-    coefs = np.random.random((dim,))
-    start = np.zeros((dim,))
-    stop = np.ones((dim)) * 10
+    import os
 
-    x = np.linspace(start, stop, 100)
-    noise = np.random.random(x.shape)
-    data = coefs * (x + noise)
 
-    #X = np.((int(data_size), 10))
-    start = time.time()
-    m, c, mesh_sizes, d_vals = mesh_dim(data, .01)
+    def identity(rews, obs, acts):
+        return rews
 
-    xdata = np.log2(d_vals)
-    ydata = np.log2(mesh_sizes)
 
-    plt.plot(d_vals, mesh_sizes, 'x-')
+    def vardiv(rews, obs, acts):
+        return rews / variation_dim(obs)
+
+
+    def varmul(rews, obs, acts):
+        return rews * variation_dim(obs)
+
+
+    def rough_measure(x):
+        d = torch.stack([x[t, :] - x[t - 1, :] for t in range(1, x.shape[0])])
+        return d.std()  # $/(torch.abs(d.mean()))
+
+
+    def rmul(rews, obs, acts):
+        return rews * rough_measure(obs)
+
+
+    def rdiv(rews, obs, acts):
+        return rews / rough_measure(obs)
+
+
+    def pcastd(rews, obs, acts):
+        pca = PCA()
+        pca.fit((obs - obs.mean()) / obs.std())
+        return rews * pca.explained_variance_ratio_.std()
+
+
+    def mdim_mul(rews, obs, acts):
+        m, _, _, _ = mesh_dim(obs)
+        return m * rews
+
+
+    def mdim_div(rews, obs, acts):
+        m, _, _, _ = mesh_dim(obs)
+        return rews / m
+
+
+    def cdim_mul(rews, obs, acts):
+        _, c, _, _ = mesh_dim(obs)
+        return c * rews
+
+
+    def cdim_div(rews, obs, acts):
+        _, c, _, _ = mesh_dim(obs)
+        return rews / c
+
+
+    def madodiv(rews, obs, acts):
+        return rews / variation_dim(obs, order=1)
+
+
+    def variodiv(rews, obs, acts):
+        return rews / variation_dim(obs, order=2)
+
+
+    def radodiv(rews, obs, acts):
+        return rews / variation_dim(obs, order=.5)
+
+
+    def do_rollout(env, policy, render=False):
+        torch.autograd.set_grad_enabled(False)
+
+        act_list = []
+        obs_list = []
+        rew_list = []
+
+        dtype = torch.float32
+        obs = env.reset()
+        done = False
+        cur_step = 0
+
+        while not done:
+            obs = torch.as_tensor(obs, dtype=dtype).detach()
+            obs_list.append(obs.clone())
+
+            act = policy(obs)
+            obs, rew, done, _ = env.step(act.numpy())
+            if render:
+                env.render()
+                time.sleep(.01)
+
+            act_list.append(torch.as_tensor(act.clone()))
+            rew_list.append(rew)
+
+            cur_step += 1
+
+        ep_length = len(rew_list)
+        ep_obs = torch.stack(obs_list)
+        ep_act = torch.stack(act_list)
+        ep_rew = torch.tensor(rew_list, dtype=dtype)
+        ep_rew = ep_rew.reshape(-1, 1)
+
+        torch.autograd.set_grad_enabled(True)
+        return ep_obs, ep_act, ep_rew, ep_length
+
+
+    def do_long_rollout(env, policy, ep_length):
+        torch.autograd.set_grad_enabled(False)
+
+        act_list = []
+        obs_list = []
+        rew_list = []
+
+        dtype = torch.float32
+        obs = env.reset()
+        done = False
+        cur_step = 0
+
+        while cur_step < ep_length:
+            obs = torch.as_tensor(obs, dtype=dtype).detach()
+            obs_list.append(obs.clone())
+
+            act = policy(obs)
+            obs, rew, done, _ = env.step(act.numpy())
+
+            act_list.append(torch.as_tensor(act.clone()))
+            rew_list.append(rew)
+
+            cur_step += 1
+
+        ep_length = len(rew_list)
+        ep_obs = torch.stack(obs_list)
+        ep_act = torch.stack(act_list)
+        ep_rew = torch.tensor(rew_list, dtype=dtype)
+        ep_rew = ep_rew.reshape(-1, 1)
+
+        torch.autograd.set_grad_enabled(True)
+        return ep_obs, ep_act, ep_rew, ep_length
+
+
+    env_name = "HalfCheetah-v2"
+
+    num_experiments = 2
+    data = torch.load("/home/sgillen/work/lorenz/run_ars/data_mcshdim4/HalfCheetah-v2.xr")
+    policy_dict = data.policy_dict
+
+    exp_names = [fn.__name__ for fn in data.attrs['post_fns']]
+    num_seeds = len(policy_dict[exp_names[0]])
+
+    combined_curves = rewards = xr.DataArray(np.zeros((num_experiments, num_seeds, 1000)),
+                                             dims=("post", "trial", "epoch"),
+                                             coords={"post": exp_names})
+
+    rews = data.rews  # /data.post_rews
+    combined_curves[:, :, 750:] = rews
+
+    data = torch.load("/home/sgillen/work/lorenz/run_ars/data17/HalfCheetah-v2.xr")
+    policy_dict['identity'] = data.policy_dict['identity']
+
+    rews = data.rews  # /data.post_rews
+    combined_curves[:, :, :750] = rews.loc['madodiv']
+
+    means = combined_curves.mean(dim="trial")
+    stds = combined_curves.std(dim="trial")
+
+    plt.plot(means.T)
+    plt.legend(exp_names, loc='upper left')
+    ci = stds
+
+    for mean, c in zip(means, ci):
+        plt.fill_between([t for t in range(len(mean))], (mean - c), (mean + c), alpha=.5)
+    plt.title("Preprocessed Reward")
     plt.show()
-    plt.plot(xdata, ydata, 'x-')
+
+    seed = 2
+    ep_length = 10000
+    policy = policy_dict['identity'][seed]
+    env = gym.make(env_name)
+    o, a, r, _ = do_long_rollout(env, policy, ep_length=ep_length)
+    # o,a,r,l = do_rollout(env, policy, render=True)
+
+    plt.plot(o)
     plt.show()
+
+    target = o[200:]
+    target = (target - policy.state_means) / policy.state_std
+    # target = (target - target.mean(dim=0))/target.std(dim=0)
+
+    print(sum(r))
+    m, c, l, d = mesh_dim(target)
+    print(m)
+    print(c)
+
+    # ==============
+    policy = policy_dict['mdim_div'][seed]
+    #% time
+    o2, a2, r2, _ = do_long_rollout(env, policy, ep_length=ep_length)
+    # o2,a2,r2,l2 = do_rollout(env, policy, render=True)
+    plt.plot(o2)
+    plt.figure()
+
+    target2 = o2[200:]
+    target2 = (target2 - policy.state_means) / policy.state_std
+    # target2 = (target2 - target2.mean(dim=0))/target2.std(dim=0)
+
+    # target = (target - policy.state_means)/policy.state_std
+
+    print(sum(r2))
+    m2, c2, l2, d2 = mesh_dim(target2)
+    print(m2)
+    print(c2)
