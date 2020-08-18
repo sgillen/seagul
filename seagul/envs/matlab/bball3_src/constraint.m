aacc1a04352eb914aa33e679e002ca3ccc4a03eb
sgillen <sgillen@ucsb.edu>
2020-08-17 20:11:58 -0700

    added bbal3 src as a submodule
    Former-commit-id: df36d79e5d64a77cb5ee13e71994616ae28b50d1
    Former-commit-id: cd52e2480f5945316680f087d6cec09411cd6d81 [formerly f9eb1c1e531da20e9426ff34d1e4d72663a85e17] [formerly e0c5f904bb7d05dc4a56c08fc560b1d5f03d6ce8 [formerly cc859dfbc40f874cc54771fe668ce904ca249291]]
    Former-commit-id: f1da096ab52c1c6b6aa947a6d447c5f8b665bfbd [formerly 699a1fdd520c566f19d5ef49601d39e9303f070f]
    Former-commit-id: 2befbf6f0c78f629d898ebe3142980fe608c2537

---
+++ seagul/envs/matlab/bball3_src/constraint.m
@@ -0,0 +1,43 @@
+%% Function to impose constraints on motion
+function flag = constraint(X)
+
+q1 = X(1); q2 = X(2); q3 = X(3); xb = X(4); yb = X(5);
+dq1 = X(6); dq2 = X(7); dq3 = X(8); vbx = X(9); vby = X(10);
+
+th1 = q1;
+th2 = q1 + q2;
+th3 = q1 + q2 + q3;
+thetaPerp = th3 + pi/2;
+
+params;
+
+x1 =  -l1*sin(th1);
+x1cm = -p1*sin(th1);
+x2 = x1 - l2*sin(th2);
+x2cm = x1 - p2*sin(th2);
+x3 = x2 - l3*sin(th3);
+x3cm = x2 - p3*sin(th3);
+
+y1 = l1*cos(th1);
+y1cm = p1*cos(th1);
+y2 = y1 + l2*cos(th2);
+y2cm = y1 + p2*cos(th2);
+y3 = y2 + l3*cos(th3);
+y3cm = y2 + p3*cos(th3);
+
+if yb < y3
+    %condition = "Ball below Link 2";
+    flag = 4;
+elseif y3 < 0
+    %condition = "Link1 below ground";
+    flag = 3;
+elseif y2 < 0
+    %condition = "Link2 below ground";
+    flag = 2;
+elseif y1 < 0
+    %condition = "Link1 below ground";
+    flag = 1;
+else
+    flag = 0;
+    %condition = "No Failure";
+end
