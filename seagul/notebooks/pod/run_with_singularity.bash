e105f656ce8cb037a4a51b1c837941a0af661b0f
sgillen <sgillen@ucsb.edu>
2020-08-26 15:16:07 -0700

    re-removed old/ and notebooks/

--- seagul/notebooks/pod/run_with_singularity.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -l
-#SBATCH -N 1 --ntasks-per-node=20
-#SBATCH --mail-user=grabka@ucsb.edu
-#SBATCH --mail-type=start,end
-cd seagul
-git pull origin rbf
-cd ..
-module load singularity
-singularity exec $HOME/rbf.simg python3.7 $1
--- seagul/old/simulink_bots/write_weights.m
+++
@@ -1,5 +0,0 @@
-function write_weights(file_name, A)
-    f = fopen(file_name, 'w');
-    fwrite(f,A,'float64');
-end
-
