3aec1016d7fdd9972d5254350a7209df60365dfc
sgillen <sgillen@ucsb.edu>
2021-01-29 19:46:05 -0800

    updates to hmap env

--- seagul/envs/__init__.py
+++ seagul/envs/__init__.py
@@ -8,7 +8,7 @@ register(id="mj_su_cartpole_discrete-v0", entry_point="seagul.envs.mujoco:MJSUCa
 register(id="five_link-v3", max_episode_steps=1000, entry_point="seagul.envs.mujoco:FiveLinkWalkerEnv")
 register(id="det_humanoid-v1", entry_point="seagul.envs.mujoco:DetHumanoidEnv", max_episode_steps=1000)
 register(id="humanoid_long-v1", entry_point="gym.envs.mujoco:HumanoidEnv")
-register(id="hmap_hopper-v0", entry_point="seagul.envs.mujoco:HmapHopperEnv")
+register(id="hmap_hopper-v0", entry_point="seagul.envs.mujoco:HmapHopperEnv", max_episode_steps=1000)
 
 register(id="lorenz-v0", entry_point="seagul.envs.simple_nonlinear:LorenzEnv")
 register(id="linear_z-v0", entry_point="seagul.envs.simple_nonlinear:LinearEnv")
